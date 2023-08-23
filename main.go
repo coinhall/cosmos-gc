7211356753a3701ee1361e0e46a05c91185bdd2d
Aaron Choo <choozy.aaron@gmail.com>
2023-08-23 16:43:34 +0800

    fix: create new db instead of deleting records

--- main.go
+++ main.go
@@ -11,22 +11,20 @@ import (
 	"github.com/cometbft/cometbft/types"
 	"github.com/cosmos/gogoproto/proto"
 	"github.com/syndtr/goleveldb/leveldb"
-	"github.com/syndtr/goleveldb/leveldb/opt"
 	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
 func pruneBlockstoreState(dataDir string) {
-	db, err := leveldb.OpenFile(filepath.Join(dataDir, "blockstore.db"), nil)
+	dbCurrent, err := leveldb.OpenFile(filepath.Join(dataDir, "blockstore.db"), nil)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
 
 	// Get latest height
 	fmt.Printf("Finding latest block height...\n")
 	prefix := []byte("H:")
 	latestHeight := uint64(0)
-	iter := db.NewIterator(util.BytesPrefix(prefix), nil)
+	iter := dbCurrent.NewIterator(util.BytesPrefix(prefix), nil)
 	for iter.Next() {
 		height, err := strconv.ParseUint(string(iter.Key())[len(prefix):], 10, 64)
 		if err != nil {
@@ -41,7 +39,7 @@ func pruneBlockstoreState(dataDir string) {
 
 	// Get blockhash of latest height
 	fmt.Printf("Finding latest block hash...\n")
-	value, err := db.Get([]byte("H:"+fmt.Sprint(latestHeight)), nil)
+	value, err := dbCurrent.Get([]byte("H:"+fmt.Sprint(latestHeight)), nil)
 	if err != nil {
 		panic(err)
 	}
@@ -56,30 +54,76 @@ func pruneBlockstoreState(dataDir string) {
 	latestHash := strings.ToLower(blockMeta.BlockID.Hash.String())
 	fmt.Printf("Latest block hash [%v]\n", latestHash)
 
-	// Delete all keys except for the latest heights
-	fmt.Printf("Deleting blockstore.db state till latest height and hash...\n")
-	iter = db.NewIterator(nil, nil)
-	maxHeightString := fmt.Sprint(latestHeight)
-	batch := new(leveldb.Batch)
-	for iter.Next() {
-		key := string(iter.Key())
-		if key != "H:"+maxHeightString &&
-			key != "C:"+maxHeightString &&
-			key != "P:"+maxHeightString+":0" &&
-			key != "SC:"+maxHeightString &&
-			key != "BH:"+latestHash &&
-			key != "blockStore" {
-			batch.Delete([]byte(key))
-		}
-	}
-	iter.Release()
-	opts := opt.WriteOptions{
-		Sync: true,
-	}
-	if err := db.Write(batch, &opts); err != nil {
+	// Create new blockstore.db and populate latest height and hash
+	fmt.Printf("Creating new db and adding latest info from old db...\n")
+	if err := os.RemoveAll(filepath.Join(dataDir, "blockstore.new.db")); err != nil {
 		panic(err)
 	}
-	fmt.Printf("Successfully pruned blockstore.db till height [%v]\n", latestHeight)
+	dbNew, err := leveldb.OpenFile(filepath.Join(dataDir, "blockstore.new.db"), nil)
+	if err != nil {
+		panic(err)
+	}
+	var (
+		hKey          []byte = []byte("H:" + fmt.Sprint(latestHeight))
+		hVal          []byte
+		cKey          []byte = []byte("C:" + fmt.Sprint(latestHeight-1))
+		cVal          []byte
+		pKey          []byte = []byte("P:" + fmt.Sprint(latestHeight) + ":0")
+		pVal          []byte
+		scKey         []byte = []byte("SC:" + fmt.Sprint(latestHeight))
+		scVal         []byte
+		bhKey         []byte = []byte("BH:" + latestHash)
+		bhVal         []byte
+		blockstoreKey []byte = []byte("blockStore")
+		blockstoreVal []byte
+	)
+	hVal, err = dbCurrent.Get(hKey, nil)
+	if err != nil {
+		panic(err)
+	}
+	cVal, err = dbCurrent.Get(cKey, nil)
+	if err != nil {
+		panic(err)
+	}
+	pVal, err = dbCurrent.Get(pKey, nil)
+	if err != nil {
+		panic(err)
+	}
+	scVal, err = dbCurrent.Get(scKey, nil)
+	if err != nil {
+		panic(err)
+	}
+	bhVal, err = dbCurrent.Get(bhKey, nil)
+	if err != nil {
+		panic(err)
+	}
+	blockstoreVal, err = dbCurrent.Get(blockstoreKey, nil)
+	if err != nil {
+		panic(err)
+	}
+	batch := new(leveldb.Batch)
+	batch.Put(hKey, hVal)
+	batch.Put(cKey, cVal)
+	batch.Put(pKey, pVal)
+	batch.Put(scKey, scVal)
+	batch.Put(bhKey, bhVal)
+	batch.Put(blockstoreKey, blockstoreVal)
+	if err := dbNew.Write(batch, nil); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Successfully added latest info to new blockstore.db\n")
+
+	// Remove old db and rename new db
+	fmt.Printf("Removing old db and renaming new db...\n")
+	dbNew.Close()
+	dbCurrent.Close()
+	if err := os.RemoveAll(filepath.Join(dataDir, "blockstore.db")); err != nil {
+		panic(err)
+	}
+	if err := os.Rename(filepath.Join(dataDir, "blockstore.new.db"), filepath.Join(dataDir, "blockstore.db")); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Successfully pruned blockstore.db!\n")
 }
 
 func main() {
